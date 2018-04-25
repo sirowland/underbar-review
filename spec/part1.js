592f31eedf1a24f26e3d1049dc2503d87501c954
sirowland <srowland2594@gmail.com>
2018-04-24 20:41:47 -0700

    Finished part1!

--- spec/part1.js
+++ spec/part1.js
@@ -332,7 +332,7 @@
         var iterator = function(value) { return value === 1; };
         var numbers = [1, 2, 2, 3, 4, 4];
 
-        expect(_.uniq(FILL_ME_IN)).to.eql([1, 2]);
+        expect(_.uniq(numbers, true, iterator)).to.eql([1, 2]);
       });
 
       it('should produce a brand new array instead of modifying the input array', function() {
@@ -376,7 +376,7 @@
       });
 
       it('should apply a function to every value in an array', function() {
-        var multiplyByTwo = FILL_ME_IN;
+        var multiplyByTwo = function(value) {return value * 2};
 
         expect(_.map([1, 2, 3], multiplyByTwo)).to.eql([2, 4, 6]);
       });
@@ -399,7 +399,7 @@
           { name: 'curly', age: 50 }
         ];
 
-        expect(_.pluck(people, 'name')).to.FILL_ME_IN(['moe', 'curly']);
+        expect(_.pluck(people, 'name')).to.eql(['moe', 'curly']);
       });
 
       it('should not modify the original array', function() {
@@ -410,7 +410,7 @@
 
         _.pluck(people, 'name');
 
-        expect(people).to.FILL_ME_IN([{ name: 'moe', age: 30 }, { name: 'curly', age: 50 }]);
+        expect(people).to.eql([{ name: 'moe', age: 30 }, { name: 'curly', age: 50 }]);
       });
     });
 
@@ -470,6 +470,7 @@
           // FILL_ME_IN
           // Add a line here that makes this test pass
           // for a working implementation of reduce
+          orderTraversed.push(item);
           return memo;
         }, 10);
 
@@ -499,7 +500,7 @@
         expect(result).to.equal(4);
       });
 
-      it('Fill me in with a description of the behavior this test is checking for', function() {
+      it('should multiply each element by each other starting with 0 resulting in 0', function() {
         var result = _.reduce([1, 2, 3], function(memo, item) {
           return memo * item;
         }, 0);
--- src/underbar.js
+++ src/underbar.js
@@ -145,6 +145,12 @@
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
+     var arr = [];
+     _.each(collection, function(value) {
+      arr.push(iterator(value));    
+     // body...
+     });
+    return arr;
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
@@ -189,6 +195,18 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    if (!accumulator && accumulator !== 0) {
+      accumulator = collection[0];
+      var obj = collection.slice(1);
+    } else {
+      var obj = collection;
+    }
+
+    _.each(obj, function(value) {
+        accumulator = iterator(accumulator, value);
+    });
+                              
+    return accumulator;                           
   };
 
   // Determine if the array or object contains a given value (using `===`).
